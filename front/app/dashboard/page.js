ac633f2b690f3dfc15bdc98f15af6921f2d6a8bc
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2025-02-04 10:06:30 -0300

    modifico app/globals.css y a agrego app/dashboard

---
+++ front/app/dashboard/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const dashboard = () => {
+  return (
+    <div>
+        <h1>Este es el perfil del usuario</h1>
+        <div>
+            <h2>Aqui estara la informacion del usuario</h2>
+        </div>
+    </div>
+  )
+}
+
+export default dashboard
--- front/app/page.js
+++ front/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
 <main>
   <h1>My First SaaS</h1>
+  <h2>Home</h2>
   <button className="btn">Get Started</button>
 </main>
   );
