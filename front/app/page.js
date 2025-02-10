42efb05b85097afc92c84b129a903b2da2fa4898
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2025-02-10 10:16:20 -0300

    mejorando formulario de register y login

--- front/app/page.js
+++ front/app/page.js
@@ -1,15 +1,15 @@
 export default function Home() {
   return (
     <main>
-      <section>
-        <h1 className="text-center text-xl my-6 font-bold">My First SaaS</h1>
+      <section className="p-4 my-4">
+        <h1 className="text-center text-xl font-bold">My First SaaS</h1>
       </section>
 
-      <section className="p-4">
+      <section className="p-4 my-4">
         <p className="text-lg">Esta es una sección del home</p>
       </section>
 
-      <section className="p-4">
+      <section className="p-4 my-4">
         <p className="text-lg">Esta es otra sección del home</p>
       </section>
 
