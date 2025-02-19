9184649eb24274de862f9f2b27183ef3fddf66e0
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2025-02-19 11:23:20 -0300

    estilado de contador

--- front/components/Contador.js
+++ front/components/Contador.js
@@ -14,11 +14,12 @@ const Contador = () => {
 
   return (
     <div>
-      <p className='m-6 font-bold text-lg'>PRACTICA DE HOOK USESTATE DE REACT</p>
+      <h2 className='m-4 font-bold text-lg'>PRACTICA DE HOOK USESTATE DE REACT</h2>
+      <h3>Es una función especial para crear variables con estado. Ésta se actualiza y cambia el estado inicial.</h3>
       <div className='flex justify-center'>
-      <button className='text-md border border-purple-800 p-4' onClick={restarCounter}>restar</button>
-      <div className='p-6 text-cyan-800 font-semibold text-lg'>{counter}</div>
-      <button className='text-md border border-purple-800 p-4' onClick={sumarCounter}>sumar</button>
+      <button className='text-base border-2 border-purple-800 px-2 py-2 rounded-md font-semibold hover:bg-purple-100 transition' onClick={restarCounter}>RESTAR</button>
+      <div className='p-6 text-pink-700 font-semibold text-2xl mx-5'>{counter}</div>
+      <button className='text-base border-2 border-purple-800 px-2 py-2 rounded-md font-semibold hover:bg-purple-100 transition' onClick={sumarCounter}>SUMAR</button>
       </div>
     </div>
   )
